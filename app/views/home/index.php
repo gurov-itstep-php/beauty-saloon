3979d12af825169319382de3388001adf5e93dc7
Anatolii <gurov.anatoliy@gmail.com>
2021-08-13 18:37:14 +0300

    Дорбавлен файл конфигурации database.php

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,19 +1,7 @@
 <h3 style="color: red">
     Контент главной страницы<br>
 </h3>
-    <?php
-        var_dump($par1);
-        echo('<br>');
-    ?>
-    <?php
-        var_dump($par2);
-        echo('<br>');
-    ?>
-    <?php
-        var_dump($par3);
-        echo('<br>');
-    ?>
-    <?php
-        var_dump($par4);
-        echo('<br>');
-    ?>
+    <?php var_dump($par1); ?><br>
+    <?php var_dump($par2); ?><br>
+    <?php var_dump($par3); ?><br>
+    <?php var_dump($par4); ?><br>
--- index.php
+++ index.php
@@ -10,8 +10,8 @@ require_once('sys/lib/autoloader.php');
 $testObject = new \sys\core\Test;
 $baseController = new \sys\core\Controller;
 */
-$homeController = new \app\controllers\Home;
-$homeController->index();
+//$homeController = new \app\controllers\Home;
+//$homeController->index();
 /*
 $homeController->about();
 $homeController->contact();
