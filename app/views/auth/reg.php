1a6fe400a8c2194c7e6c76b133fddece1c62b165
Anatolii <gurov.anatoliy@gmail.com>
2021-08-15 18:58:37 +0300

    Реализация логики регистрации пользователей сайта

---
+++ app/views/auth/reg.php
@@ -0,0 +1,6 @@
+<div style="padding-top: 120px; text-align: center;">
+    <h2>Регистрация</h2>
+    <div class="form-box">
+        <?php $form->generate(); ?>
+    </div>
+</div>
--- app/views/includes/nav.php
+++ app/views/includes/nav.php
@@ -2,7 +2,7 @@
          <div class="header">
             <div class="container">
                <div class="row">
-                  <div class="col-xl-5 col-lg-5 col-md-9 col-sm-9">
+                  <div class="col-xl-6 col-lg-5 col-md-9 col-sm-9">
                      <nav class="navigation navbar navbar-expand-md navbar-dark ">
                         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
                            <span class="navbar-toggler-icon"></span>
@@ -37,11 +37,10 @@
                         </div>
                      </div>
                   </div>
-                  <div class="col-xl-5 col-lg-5 col-md-5 col-sm-5">
+                  <div class="col-xl-4 col-lg-5 col-md-5 col-sm-5">
                      <ul class="email">
-                        <li><a href="#">Call: (+71) 1234567890</a></li>
-                        <li><a href="#">Email: demo@gmail.com</a></li>
-                        <li><a href="#"><i class="fa fa-search" aria-hidden="true"></i></a></li>
+                        <li><a href="<?= self::ROOT ?>/auth/reg">Register</a></li> | 
+                        <li><a href="<?= self::ROOT ?>/auth/entry">Login</a></li>
                      </ul>
                   </div>
                </div>
